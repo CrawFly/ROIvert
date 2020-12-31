f8b0483bb34152136e35f1a726e35ced7e6aa115
dave <dbulkin@hotmail.com>
2020-12-31 11:09:50 -0500

    ContrastWidget move impl classes out

--- src/widgets/ContrastWidget.cpp
+++ src/widgets/ContrastWidget.cpp
@@ -4,216 +4,18 @@
 #include <QApplication>
 #include <QGraphicsSceneMouseEvent>
 #include <QBoxLayout>
+#include "ContrastWidgetImpl.h"
+#include <QDoubleSpinBox>
 
 #include <QDebug>
 
 
-namespace impl {
-
-    VertLine::VertLine(QGraphicsScene* scene) {
-        if (scene) {
-            scene->addItem(this);
-        }
-        setCursor(Qt::SizeHorCursor);
-        setColor(QColor("#2274A5"));
-        setX(X, true);
-    }
-    void VertLine::setColor(QColor clr, qreal lw) {
-        QPen pen(clr, lw);
-        pen.setCosmetic(true);
-        setPen(pen);
-    }
-    void VertLine::setX(qreal x, bool silent) {
-        if (x >= minX && x <= maxX) {
-            X = x;
-            setLine(X, 0, X, 1);
-            if (!silent) {
-                emit changeVal(X);
-            }
-        }
-    }
-    void VertLine::setMin(qreal m) { minX = m; }
-    void VertLine::setMax(qreal m) { maxX = m; }
-    void VertLine::mousePressEvent(QGraphicsSceneMouseEvent* event) {
-    }
-    void VertLine::mouseMoveEvent(QGraphicsSceneMouseEvent* event) {
-        setX(event->pos().x());
-    }
-    QRectF VertLine::boundingRect() const {
-        return QRectF(X - .025, 0, .05, 1);
-    };
-
-    GammaLine::GammaLine(QGraphicsScene* scene) {
-        if (scene) {
-            scene->addItem(this);
-        }
-        setCursor(Qt::SizeVerCursor);
-        setColor(QColor("#2274A5"));
-        setGamma(Gamma);
-    }
-    void GammaLine::setColor(QColor clr, qreal lw) {
-        QPen pen(clr, lw);
-        pen.setCosmetic(true);
-        setPen(pen);
-    }
-    void GammaLine::setMin(qreal m) { minX = m; updateLine(); }
-    void GammaLine::setMax(qreal m) { maxX = m; updateLine(); }
-    void GammaLine::setMinGamma(qreal m) { mingamma = m; }
-    void GammaLine::setMaxGamma(qreal m) { maxgamma = m; }
-    void GammaLine::setGamma(qreal g, bool silent) {
-        if (g >= mingamma && g <= maxgamma) {
-            Gamma = g;
-            updateLine();
-            if (!silent) {
-                emit changeVal(Gamma);
-            }
-        }
-    }
-    void GammaLine::mousePressEvent(QGraphicsSceneMouseEvent* event) {}
-    void GammaLine::mouseMoveEvent(QGraphicsSceneMouseEvent* event) {
-        QPointF newloc = event->pos();
-
-        // Cap x/y to minmax
-        newloc.setX(qMin(qMax(newloc.x(), minX), maxX));
-        newloc.setY(1 - qMin(qMax(newloc.y(), 0.), 1.));
-
-        // Convert x to be the distance between min and max
-        newloc.setX((newloc.x() - minX) / (maxX - minX));
-
-        // Solve for gamma and update:
-        setGamma(log(newloc.y()) / log(newloc.x()));
-    }
-    void GammaLine::updateLine() {
-        float xdelt = maxX - minX;
-        QPointF startpoint(minX, 1);
-
-        QPainterPath pth(startpoint);
-
-        for (int i = 0; i <= 100; ++i) {
-            float x = i / 100.;  // this is x for the gamma equation
-            QPointF p(x * xdelt + minX, 1 - (pow(x, Gamma)));
-            pth.lineTo(p);
-        }
-
-        setPath(pth);
-    }
-    QPainterPath GammaLine::shape() const {
-        QPainterPath pth, rpth;
-        pth = path();
-        rpth = path().toReversed();
-
-        pth.translate(0, .025);
-        rpth.translate(0, -.025);
-
-        pth.connectPath(rpth);
-        return pth;
-    }
-
-    ContrastChart::ContrastChart(QWidget* parent)
-        : QGraphicsView(parent)
-    {
-        QGraphicsScene* scene = new QGraphicsScene(0, 0, 1, 1);
-        setRenderHints(QPainter::Antialiasing);
-
-        setScene(scene);
-        setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-        setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-
-        // Histogram
-        HistPath = new QGraphicsPathItem;
-        setHistogramColor(Qt::black, 2);
-        scene->addItem(HistPath);
-
-        // Overlays to mark out of min/max
-        QGraphicsRectItem* leftOverlayRect = new QGraphicsRectItem;
-        leftOverlayRect->setRect(0, 0, 0, 1);
-        leftOverlayRect->setPen(QPen(Qt::NoPen));
-        leftOverlayRect->setBrush(QBrush(QColor(255, 255, 255, 200)));
-        scene->addItem(leftOverlayRect);
-        QGraphicsRectItem* rightOverlayRect = new QGraphicsRectItem;
-        rightOverlayRect->setRect(1, 0, 0, 1);
-        rightOverlayRect->setPen(QPen(Qt::NoPen));
-        rightOverlayRect->setBrush(QBrush(QColor(255, 255, 255, 200)));
-        scene->addItem(rightOverlayRect);
-
-        // min/max lines
-        minline = new VertLine(scene);
-        minline->setX(0.);
-        minline->setMin(0.);
-        minline->setMax(1.);
-        maxline = new VertLine(scene);
-        maxline->setX(1.);
-        maxline->setMin(0.);
-        maxline->setMax(1.);
-
-        // gamma curve
-        gamline = new GammaLine(scene);
-
-        // connect connect min/max/gamma with eachother
-        connect(minline, &VertLine::changeVal, maxline, &VertLine::setMin);
-        connect(maxline, &VertLine::changeVal, minline, &VertLine::setMax);
-        connect(minline, &VertLine::changeVal, gamline, &GammaLine::setMin);
-        connect(maxline, &VertLine::changeVal, gamline, &GammaLine::setMax);
-
-        // connect overlays to accompanying lines
-        connect(minline, &VertLine::changeVal, this, [leftOverlayRect](qreal val) {leftOverlayRect->setRect(0, 0, val, 1); });
-        connect(maxline, &VertLine::changeVal, this, [rightOverlayRect](qreal val) {rightOverlayRect->setRect(val, 0, 1. - val, 1); });
-
-        // connect outbound signal:
-        auto lam = [this] {emit contrastChanged(getValues()); };    //connect(minline,&VertLine::changeVal,this,[]
-        connect(minline, &VertLine::changeVal, this, lam);
-        connect(maxline, &VertLine::changeVal, this, lam);
-        connect(gamline, &GammaLine::changeVal, this, lam);
-    }
-    void ContrastChart::setHistogram(QVector<float> y) {
-        QPainterPath pth;
-        const float ymax = *std::max_element(y.constBegin(), y.constEnd());
-
-        pth.moveTo(0, 1);
-        for (int i = 0; i < y.size(); i++) {
-            pth.lineTo((float)i / y.size(), 1. - y[i] / ymax);
-        }
-        pth.lineTo(1, 1);
-        HistPath->setPath(pth);
-    }
-    void ContrastChart::setHistogramColor(QColor clr, qreal linewidth, bool dogradfill) {
-        QPen pen(clr, linewidth);
-        pen.setCosmetic(true);
-        HistPath->setPen(pen);
-        if (dogradfill) {
-            QLinearGradient lGrad(.5, 1, .5, 0);
-            lGrad.setColorAt(0, Qt::white);
-            clr.setAlpha(20);
-            lGrad.setColorAt(.5, clr);
-            clr.setAlpha(70);
-            lGrad.setColorAt(1, clr);
-            HistPath->setBrush(lGrad);
-        }
-    }
-    void ContrastChart::resizeEvent(QResizeEvent* event) {
-        fitInView(scene()->sceneRect(), Qt::IgnoreAspectRatio);
-    }
-    void ContrastChart::setLineColor(QColor clr, qreal linewidth) {
-        minline->setColor(clr, linewidth);
-        maxline->setColor(clr, linewidth);
-        gamline->setColor(clr, linewidth);
-    }
-    void ContrastChart::setValues(ROIVert::contrast minmaxgamma) {
-        // Silent setter
-        minline->setX(std::get<0>(minmaxgamma), false);
-        maxline->setX(std::get<1>(minmaxgamma), false);
-        gamline->setGamma(std::get<2>(minmaxgamma), false);
-    }
-    void ContrastChart::setGammaRange(qreal mingamma, qreal maxgamma) {
-        gamline->setMinGamma(mingamma);
-        gamline->setMaxGamma(maxgamma);
-    }
-    ROIVert::contrast ContrastChart::getValues() {
-        return std::make_tuple(minline->getX(), maxline->getX(), gamline->getGamma());
-    }
-}
-
 ContrastWidget::ContrastWidget(QWidget* parent) : QWidget(parent) {
+    chart = new ContrastWidgetImpl::ContrastChart;
+    spinMin = new QDoubleSpinBox;
+    spinGamma = new QDoubleSpinBox;
+    spinMax = new QDoubleSpinBox;
+
     auto lay = new QVBoxLayout(this);
     lay->addWidget(chart);
     chart->setToolTip(tr("A histogram of image data is shown. Slide the minimum/maximum/gamma lines to adjust contrast or set values in the boxes below."));
@@ -258,6 +60,14 @@ ContrastWidget::ContrastWidget(QWidget* parent) : QWidget(parent) {
         spinMax->setValue(std::get<1>(c));
         spinGamma->setValue(std::get<2>(c)); };
 
-    connect(chart, &impl::ContrastChart::contrastChanged, this, lamChart2Spin);
+    connect(chart, &ContrastWidgetImpl::ContrastChart::contrastChanged, this, lamChart2Spin);
 
 }
+
+// These are all passthru to chart
+void ContrastWidget::setContrast(ROIVert::contrast c) { chart->setValues(c); }
+ROIVert::contrast ContrastWidget::getContrast() { return chart->getValues(); }
+void ContrastWidget::setHistogram(QVector<float> y) { chart->setHistogram(y); }
+void ContrastWidget::setHistogramColor(QColor c) { chart->setHistogramColor(c); }
+void ContrastWidget::setLineColor(QColor c) { chart->setLineColor(c); }
+void ContrastWidget::setGammaRange(qreal mingamma, qreal maxgamma) { chart->setGammaRange(mingamma, maxgamma); };
--- src/widgets/ContrastWidget.h
+++ src/widgets/ContrastWidget.h
@@ -1,88 +1,12 @@
 #pragma once
-#include <QGraphicsView>
-#include <QGraphicsPathItem>
-#include <QGraphicsLineItem>
-#include <QDoubleSpinBox>
 #include <QWidget>
-
 #include "roivertcore.h"
 
-namespace impl {
-    class VertLine : public QObject, public QGraphicsLineItem
-    {
-        Q_OBJECT
-
-    public:
-        explicit VertLine(QGraphicsScene* scene = nullptr);
-        void setX(qreal x, bool silent = false);
-        void setMin(qreal m);
-        void setMax(qreal m);
-        void setColor(QColor clr, qreal linewidth = 3.);
-        qreal getX() { return X; };
-
-    signals:
-        void changeVal(qreal val);
-
-    protected:
-        void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
-        void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
-        QRectF boundingRect() const override;
-
-    private:
-        qreal X = 0;
-        qreal minX = 0;
-        qreal maxX = 1;
-    };
-    class GammaLine : public QObject, public QGraphicsPathItem {
-        Q_OBJECT
-
-    public:
-        explicit GammaLine(QGraphicsScene* scene = nullptr);
-        void setMin(qreal m);
-        void setMax(qreal m);
-        void setMinGamma(qreal m);
-        void setMaxGamma(qreal m);
-        void setGamma(qreal g, bool silent = false);
-        void setColor(QColor clr, qreal lw = 3.);
-        qreal getGamma() { return Gamma; }
-
-    signals:
-        void changeVal(qreal val);
-
-    protected:
-        void mousePressEvent(QGraphicsSceneMouseEvent* event) override;
-        void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override;
-        QPainterPath shape() const override;
-    private:
-        void updateLine();
-        qreal Gamma = 1., minX = 0., maxX = 1., mingamma = .001, maxgamma = 10.;
-    };
-    class ContrastChart : public QGraphicsView
-    {
-        Q_OBJECT
-
-    public:
-        ContrastChart(QWidget* parent = nullptr);
-        void setHistogram(QVector<float> y);
-        void setHistogramColor(QColor clr, qreal linewidth = 2, bool dogradfill = true);
-        void setLineColor(QColor clr, qreal linewidth = 3);
-
-        void setValues(ROIVert::contrast minmaxgamma);
-        ROIVert::contrast getValues();
-
-        void setGammaRange(qreal mingamma, qreal maxgamma);
-    signals:
-        void contrastChanged(ROIVert::contrast minmaxgamma);
-
-    protected:
-        void resizeEvent(QResizeEvent* event) override;
-    private:
-        QGraphicsPathItem* HistPath;
-        VertLine* minline;
-        VertLine* maxline;
-        GammaLine* gamline;
-    };
+namespace ContrastWidgetImpl {
+    class ContrastChart;
 }
+class QDoubleSpinBox;
+
 
 class ContrastWidget : public QWidget {
     Q_OBJECT
@@ -90,18 +14,18 @@ public:
     explicit ContrastWidget(QWidget* parent = nullptr);
 
     // These are all passthru to chart
-    void setContrast(ROIVert::contrast c) { chart->setValues(c); }
-    ROIVert::contrast getContrast() { return chart->getValues(); }
-    void setHistogram(QVector<float> y) { chart->setHistogram(y); }
-    void setHistogramColor(QColor c) { chart->setHistogramColor(c); }
-    void setLineColor(QColor c) { chart->setLineColor(c); }
-    void setGammaRange(qreal mingamma, qreal maxgamma) { chart->setGammaRange(mingamma, maxgamma); };
+    void setContrast(ROIVert::contrast c);
+    ROIVert::contrast getContrast();
+    void setHistogram(QVector<float> y);
+    void setHistogramColor(QColor c);
+    void setLineColor(QColor c);
+    void setGammaRange(qreal mingamma, qreal maxgamma);
 signals:
     void contrastChanged(ROIVert::contrast minmaxgamma);
 
 private:
-    impl::ContrastChart* chart = new impl::ContrastChart;
-    QDoubleSpinBox* spinMin = new QDoubleSpinBox;
-    QDoubleSpinBox* spinGamma = new QDoubleSpinBox;
-    QDoubleSpinBox* spinMax = new QDoubleSpinBox;
+    ContrastWidgetImpl::ContrastChart* chart;
+    QDoubleSpinBox* spinMin;
+    QDoubleSpinBox* spinGamma;
+    QDoubleSpinBox* spinMax;
 };
