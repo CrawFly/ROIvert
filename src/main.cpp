69b24b6948d276b531f5c6b326bd48339af7939d
dave <dbulkin@hotmail.com>
2021-09-02 02:07:47 -0400

    splash screen resolution adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,23 @@
 #include <QtWidgets/QApplication>
 #include <QSplashScreen>
 #include <QTimer>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QDesktopWidget* desktopWidget = qApp->desktop();
+    QRect screenGeometry = desktopWidget->screenGeometry();
+    int screenWidth = screenGeometry.width();
+    int screenHeight = screenGeometry.height();
+
     QPixmap pixmap(":/splash.png");
+    auto sf = 4000/screenWidth;
+
+    pixmap = pixmap.scaled(pixmap.width()/sf, pixmap.height()/sf);
+
+
     QSplashScreen splash(pixmap);
     splash.show();
 
